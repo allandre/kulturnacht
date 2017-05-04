9b949718cd51caf73d7fd053a9494739c4a125ab
allandre <allandre@student.ethz.ch>
2017-05-04 19:05:09 +0200

    google maps: nicer behaviour on scrolling

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var eventDate = new Date(2017, 9, 29, 18, 0, 0, 0);
+var map;
 
 var programStates = {
     undef: -1,
@@ -23,20 +24,45 @@ window.onresize = function(event) {
 
     updateProgramSection(false);
 };
+$('body').on('mousedown', function(evt) {
+	var clickInsideMap = $(evt.target).parents('#map').length > 0;
+
+	if(!clickInsideMap) {
+		setScrollingWithMouseWheel(false);
+	}
+});
+
+$(window).scroll(function() {
+	setScrollingWithMouseWheel(false);
+});
+
+function setScrollingWithMouseWheel(isEnable) {
+    map.setOptions({ scrollwheel: isEnable });
+}
+
 
 function loadMap() {
     var kuesnacht = { lat: 47.316667, lng: 8.583333 };
     var center = { lat: 47.35, lng: 8.54 };
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
-        center: center
+        center: center,
+        scrollwheel: false
     });
+
+    google.maps.event.addListener(map, 'mousedown', function() {
+        setScrollingWithMouseWheel(true);
+    });
+
 
     var marker = new google.maps.Marker({
         position: kuesnacht,
         map: map
     });
 }
+
+
+
 
 function updateProgramSection(force) {
     var containerWidth = $("#container").width();
@@ -55,7 +81,7 @@ function loadProgram(file) {
     xmlhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
             var $programDiv = $("#program");
-    		$programDiv.html(this.responseText);
+            $programDiv.html(this.responseText);
         }
     };
 
