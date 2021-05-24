81257c21a36cfb976524a877a0aa5c4372f375cb
Andreas <14872041+allandre@users.noreply.github.com>
2021-05-24 22:56:50 +0200

    reactivate navigation, add contact section

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // var map;
 
-// var hamburgerMenuWidth = 1045;
+const hamburgerMenuWidth = 1045;
 
-// var currentPosition = null;
+var currentPosition = null;
 
 // var isGalleryLoaded = false;
 // var galleryColumnCount = 1;
@@ -27,28 +27,28 @@ var index = 0;
 //     // loadMap();
 	loadImpressionGallery(index);
 
-//     // $positionIndication = $('#position-indication');
+    $positionIndication = $('#position-indication');
 
 //     // updateProgramSection(true);
 
 //     // initGallery();
  });
 
-// $(window).on('resize', function(event) {
-//     var containerWidth = $("#container").width();
-//     if (containerWidth > hamburgerMenuWidth) {
-//         hideMenu();
-//     }
+$(window).on('resize', function(event) {
+    var containerWidth = $("#container").width();
+    if (containerWidth > hamburgerMenuWidth) {
+        hideMenu();
+    }
 
-//     updateProgramSection(false);
-//     adjustGallery();
-// });
+    // updateProgramSection(false);
+    // adjustGallery();
+});
 
-// $(window).on('scroll', function() {
-//     setScrollingWithMouseWheel(false);
-//     updateNavigation();
-//     fixCloseGalleryButton();
-// });
+$(window).on('scroll', function() {
+    // setScrollingWithMouseWheel(false);
+    updateNavigation();
+    // fixCloseGalleryButton();
+});
 
 // $('body').on('mousedown', function(evt) {
 //     var clickInsideMap = $(evt.target).parents('#map').length > 0;
@@ -259,9 +259,9 @@ var index = 0;
 // }
 
 
-// function hideMenu() {
-//     $("#nav-trigger").prop("checked", false);
-// }
+function hideMenu() {
+    $("#nav-trigger").prop("checked", false);
+}
 
 
 // // calculate days until eventDate, and display on title image
@@ -285,36 +285,36 @@ function showCountdown() {
     /*$countdown.css("display", "unset");  -> IE CANNOT HANDLE THAT */
 }
 
-// function updateNavigation() {
-//     // reset state
-//     $('nav li').removeClass('current');
+function updateNavigation() {
+    // reset state
+    $('nav li').removeClass('current');
 
-//     var windowOffset = $(document).scrollTop();
-//     var windowHeight = $(window).height();
-//     var navHeight = $('nav').height();
+    var windowOffset = $(document).scrollTop();
+    var windowHeight = $(window).height();
+    var navHeight = $('nav').height();
 
-//     currentPosition = null;
-//     var $anchors = $('a.anchor');
-//     for (var i = 0; i < $anchors.length; i++) {
-//         var anchorTop = $anchors.eq(i).offset().top;
-//         if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
-//             currentPosition = { $anchor: $anchors.eq(i), index: i };
-//             if (anchorTop > windowOffset) {
-//                 // I am completely contained in the top half. Take me!
-//                 break;
-//             }
-//         } else {
-//             break;
-//         }
-//     }
+    currentPosition = null;
+    var $anchors = $('a.anchor');
+    for (var i = 0; i < $anchors.length; i++) {
+        var anchorTop = $anchors.eq(i).offset().top;
+        if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
+            currentPosition = { $anchor: $anchors.eq(i), index: i };
+            if (anchorTop > windowOffset) {
+                // I am completely contained in the top half. Take me!
+                break;
+            }
+        } else {
+            break;
+        }
+    }
 
-//     if (currentPosition !== null) {
-//         $('nav li').eq(currentPosition.index).addClass('current');
-//         history.replaceState(undefined, undefined, '#' + currentPosition.$anchor.prop('id'));
-//     } else {
-//         history.replaceState(undefined, undefined, '#');
-//     }
-// }
+    if (currentPosition !== null) {
+        $('nav li').eq(currentPosition.index).addClass('current');
+        history.replaceState(undefined, undefined, '#' + currentPosition.$anchor.prop('id'));
+    } else {
+        history.replaceState(undefined, undefined, '#');
+    }
+}
 
 
 // function toggleListRow(time) {
