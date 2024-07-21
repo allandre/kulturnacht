fdf4675888426f2b57cf9f73e8a68bc2f733e288
MiB3 <MiB3@users.noreply.github.com>
2024-07-21 12:42:39 +0200

    dont pollute global scope with script.js

--- site/script.js
+++ site/script.js
@@ -1,71 +1,76 @@
-var eventDate = new Date(2025, 8, 5, 18);
+// dont pollute global namespace. Also this way we can better check what is used inside and what outside of this script.
+(function () {
+  var eventDate = new Date(2025, 8, 5, 18);
 
-var hamburgerMenuWidth = 1045;
+  var hamburgerMenuWidth = 1045;
 
-var currentPosition = null;
-
-$(document).ready(function () {
-  showCountdown();
-});
-
-$(window).on("resize", function () {
-  var containerWidth = $("#container").width();
-  if (containerWidth > hamburgerMenuWidth) {
-    hideMenu();
+  var currentPosition = null;
+  function hideMenu() {
+    $("#nav-trigger").prop("checked", false);
   }
-});
 
-$(window).on("scroll", function () {
-  updateNavigation();
-});
+  // calculate days until eventDate, and display on title image
+  function showCountdown() {
+    // get remaining days until event
+    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+    const today = Date.now();
+    const diffDays = Math.round(Math.abs((eventDate - today) / oneDay));
 
-function hideMenu() {
-  $("#nav-trigger").prop("checked", false);
-}
+    var $countdown = $("#countdown");
+    const text = "Noch " + diffDays + " Tage bis zur";
+    $countdown.html(text);
+  }
 
-// calculate days until eventDate, and display on title image
-function showCountdown() {
-  // get remaining days until event
-  const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
-  const today = Date.now();
-  const diffDays = Math.round(Math.abs((eventDate - today) / oneDay));
+  function updateNavigation() {
+    // reset state
+    $("nav li").removeClass("current");
 
-  var $countdown = $("#countdown");
-  const text = "Noch " + diffDays + " Tage bis zur";
-  $countdown.html(text);
-}
+    var windowOffset = $(document).scrollTop();
+    var windowHeight = $(window).height();
+    var navHeight = $("nav").height();
 
-function updateNavigation() {
-  // reset state
-  $("nav li").removeClass("current");
-
-  var windowOffset = $(document).scrollTop();
-  var windowHeight = $(window).height();
-  var navHeight = $("nav").height();
-
-  currentPosition = null;
-  var $anchors = $("a.anchor");
-  for (var i = 0; i < $anchors.length; i++) {
-    var anchorTop = $anchors.eq(i).offset().top;
-    if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
-      currentPosition = { $anchor: $anchors.eq(i), index: i };
-      if (anchorTop > windowOffset) {
-        // I am completely contained in the top half. Take me!
+    currentPosition = null;
+    var $anchors = $("a.anchor");
+    for (var i = 0; i < $anchors.length; i++) {
+      var anchorTop = $anchors.eq(i).offset().top;
+      if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
+        currentPosition = { $anchor: $anchors.eq(i), index: i };
+        if (anchorTop > windowOffset) {
+          // I am completely contained in the top half. Take me!
+          break;
+        }
+      } else {
         break;
       }
+    }
+
+    if (currentPosition !== null) {
+      $("nav li").eq(currentPosition.index).addClass("current");
+      history.replaceState(
+        undefined,
+        undefined,
+        "#" + currentPosition.$anchor.prop("id"),
+      );
     } else {
-      break;
+      history.replaceState(undefined, undefined, "#");
     }
   }
 
-  if (currentPosition !== null) {
-    $("nav li").eq(currentPosition.index).addClass("current");
-    history.replaceState(
-      undefined,
-      undefined,
-      "#" + currentPosition.$anchor.prop("id"),
-    );
-  } else {
-    history.replaceState(undefined, undefined, "#");
-  }
-}
+  // global and exported stuff
+  window.addEventListener("load", () => {
+    showCountdown();
+  });
+
+  window.addEventListener("resize", () => {
+    var containerWidth = $("#container").width();
+    if (containerWidth > hamburgerMenuWidth) {
+      hideMenu();
+    }
+  });
+
+  window.addEventListener("scroll", () => {
+    updateNavigation();
+  });
+
+  window.hideMenu = hideMenu;
+})();
