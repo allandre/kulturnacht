b541bb13f037e19fcecc3e902813067aac3479ab
MiB3 <MiB3@users.noreply.github.com>
2025-05-11 14:14:13 +0200

    new hamburger menu everywhere

--- site/js/hamburger.js
+++ site/js/hamburger.js
@@ -1,9 +1,5 @@
 // Don't pollute global namespace. Also this way we can better check what is used inside and what outside of this script.
 ;(function () {
-  const hamburgerMenuWidth = 1045
-
-  let currentPosition = null
-
   const hamburgerDiv = document.querySelector('#hamburger')
   if (!hamburgerDiv) {
     // eslint-disable-next-line no-console
@@ -16,24 +12,34 @@
   linksDiv.id = 'hamburger-links'
   hamburgerDiv.appendChild(linksDiv)
   for (const anchor of document.querySelectorAll('.anchor')) {
-    const label = anchor.getAttribute('data-label')
-    if (!label) {
-      continue
-    }
+    const label = anchor.getAttribute('data-label') ?? anchor.id
 
     const link = document.createElement('a')
     link.href = `#${anchor.id}`
     link.innerHTML = label
+    link.setAttribute('data-prio', anchor.getAttribute('data-prio') ?? 0)
+    link.setAttribute('onclick', 'toggleHamburger(true)')
+    link.classList.add('actual-anchor')
     linksDiv.appendChild(link)
   }
-  const archiveLink = document.createElement('a')
-  archiveLink.href = 'site/archive.html'
-  archiveLink.innerHTML = 'Archive >'
-  linksDiv.appendChild(archiveLink)
+
+  const onArchivePage = window.location.href.includes('archive')
+
+  if (onArchivePage) {
+    const aktuellLink = document.createElement('a')
+    aktuellLink.href = '../'
+    aktuellLink.innerHTML = '< Aktuell'
+    linksDiv.prepend(aktuellLink)
+  } else {
+    const archiveLink = document.createElement('a')
+    archiveLink.href = 'site/archive.html'
+    archiveLink.innerHTML = 'Archive >'
+    linksDiv.appendChild(archiveLink)
+  }
 
   const hamburgerMenuA = document.createElement('a')
   hamburgerMenuA.href = 'javascript:void(0);'
-  hamburgerMenuA.onclick = 'toggleHamburger()'
+  hamburgerMenuA.setAttribute('onclick', 'toggleHamburger()')
   hamburgerMenuA.classList.add('hamburger-icon-wrapper')
   hamburgerDiv.appendChild(hamburgerMenuA)
 
@@ -41,67 +47,63 @@
   hamburgerIcon.classList.add('hamburger-icon')
   hamburgerMenuA.appendChild(hamburgerIcon)
 
-  function toggleHamburger() {
-    const x = document.getElementById('hamburger-links')
-    if (x.style.display === 'block') {
-      x.style.display = 'none'
-    } else {
-      x.style.display = 'block'
-    }
-  }
+  const hamburgerExpanded = linksDiv.cloneNode(true)
+  hamburgerExpanded.id = 'hamburger-expanded'
+  const navbarHeight = document
+    .querySelector('nav')
+    .getBoundingClientRect().height
+  hamburgerExpanded.style.top = `${navbarHeight}px`
+  hamburgerDiv.parentElement.appendChild(hamburgerExpanded)
 
-  function hideMenu() {
-    const navTrigger = document.getElementById('nav-trigger')
-    navTrigger.checked = false
+  function toggleHamburger(forceOff = false) {
+    const hamburgerEpanded = document.querySelector('#hamburger-expanded')
+    const currentlyHidden =
+      getComputedStyle(hamburgerEpanded).getPropertyValue('display') === 'none'
+    const hamburgerIconWrapper = document.querySelector(
+      '.hamburger-icon-wrapper'
+    )
+
+    if (currentlyHidden && !forceOff) {
+      document.querySelectorAll('#hamburger-links a').forEach(e => {
+        e.style.display = 'none'
+      })
+      hamburgerEpanded.style.display = 'flex'
+      hamburgerIconWrapper.classList.add('expanded')
+    } else {
+      hamburgerEpanded.style.removeProperty('display')
+      hamburgerIconWrapper.classList.remove('expanded')
+      reshapeMenu()
+    }
   }
 
   function updateNavigation() {
     const nav = document.getElementsByTagName('nav')[0]
 
     // reset state
-    const hamburgerLinks = nav.querySelectorAll('#hamburger-links a')
+    const hamburgerLinks = nav.querySelectorAll('#hamburger-links a.actual-anchor')
     hamburgerLinks.forEach(l => l.classList.remove('current'))
 
-    const html = document.querySelector('html')
-    const windowOffset = html.scrollTop
     const windowHeight = window.innerHeight
     const navHeight = nav.clientHeight
 
-    currentPosition = null
+    let currentPosition = null
     const anchors = Array.from(document.querySelectorAll('a.anchor'))
-
-    console.log({windowOffset})
-    console.log({windowHeight})
 
     for (let i = 0; i < anchors.length; i++) {
       const anchor = anchors[i]
       const anchorTop = anchor.getBoundingClientRect().top
 
-      console.log(anchor.id)
-      console.log({anchorTop})
-
       // I am in the lower part of the screen or deeper
-      if (anchorTop - windowOffset > (windowHeight - navHeight) / 2) {
+      if (anchorTop > (windowHeight - navHeight) / 2) {
         break
       }
 
       currentPosition = { anchor: anchor, index: i }
 
       // I am visible on screen
-      if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
+      if (anchorTop >= 0) {
         break
       }
-
-
-      // if (anchorTop - windowOffset < (windowHeight - navHeight) / 2) {
-      //   currentPosition = { anchor: anchor, index: i }
-      //   if (anchorTop > windowOffset) {
-      //     // I am completely contained in the top half. Take me!
-      //     break
-      //   }
-      // } else {
-      //   break
-      // }
     }
 
     let newHash = '#'
@@ -116,13 +118,41 @@
     }
   }
 
-  window.addEventListener('resize', () => {
-    const container = document.getElementById('container')
-    const containerWidth = container.clientWidth
-    if (containerWidth > hamburgerMenuWidth) {
-      hideMenu()
+  function reshapeMenu() {
+    const hamburgerExpanded =
+      document.querySelector('#hamburger-expanded').style.display === 'flex'
+    if (hamburgerExpanded) {
+      return
     }
-  })
+
+    const theEntries = Array.from(
+      document.querySelectorAll('#hamburger-links > a')
+    )
+    theEntries.reverse()
+    theEntries.sort((a, b) => {
+      const prioA = a.getAttribute('data-prio') ?? '0'
+      const prioB = b.getAttribute('data-prio') ?? '0'
+      return prioA - prioB
+    })
+
+    theEntries.forEach(a => a.style.removeProperty('display'))
+
+    const menuLine = document.querySelector('#hamburger')
+    const hamburger = document.querySelector('.hamburger-icon-wrapper')
+    hamburger.style.display = 'none'
+    for (const entry of theEntries) {
+      if (menuLine.scrollWidth > menuLine.getBoundingClientRect().width) {
+        entry.style.display = 'none'
+        hamburger.style.display = 'block'
+      } else {
+        break
+      }
+    }
+  }
+
+  reshapeMenu()
+
+  window.addEventListener('resize', reshapeMenu)
 
   const debounce = (callback, wait) => {
     let timeoutId = null
@@ -134,8 +164,13 @@
     }
   }
 
-  window.addEventListener('scroll', debounce(updateNavigation, 100))
+  window.addEventListener('scroll', debounce(updateNavigation, 50))
+  const resizeObserver = new ResizeObserver(() => {
+    updateNavigation()
+  })
 
-  window.hideMenu = hideMenu
+  // start observing for resize
+  resizeObserver.observe(document.querySelector('body'))
+
   window.toggleHamburger = toggleHamburger
 })()
